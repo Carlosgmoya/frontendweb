cc9e6a04503aaa60b0f047af6a838dda0d820332
LAPTOP-PH3L3CSL\cgmoy <carlosgmoya@uma.es>
2025-02-10 19:20:59 +0100

    final 25

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, session
 from authlib.integrations.flask_client import OAuth
 import requests
+from dotenv import load_dotenv
 import os
+
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
@@ -21,8 +25,37 @@ google = oauth.register(
 
 @app.route('/')
 def index():
+   # Verifica si el usuario está autenticado
+    user = session.get('user')  # Suponiendo que has guardado el usuario en la sesión
+    
+    if user:
+        autor = user.get('email')  # Usa el email del usuario como autor
+        response = requests.get(f'https://backendweb-b0ti.onrender.com/parcial/cabecera/{autor}')  # Llama a tu API
+        cabeceras = response.json()  # Suponiendo que la API devuelve una lista JSON de elementos
+
+        return render_template('index.html', user=user, cabeceras=cabeceras)
+    
+    return render_template('index.html', user=None)
+
+@app.route('/mensaje/<id>')
+def mensaje(id):
+    user = session.get('user')
+    if user:
+        # Realiza la solicitud a la API
+        response = requests.get(f'https://backendweb-b0ti.onrender.com/parcial/mensaje/{id}')
+        if response.status_code == 200:
+            data = response.json()  # Obtiene la respuesta completa como JSON
+            return render_template('mensaje.html', user=user, mensaje=data)
+        else:
+            # Manejo de error si la API no responde correctamente
+            return "Error al obtener el mensaje", 500
+    return render_template('index.html', user=None)
+
+@app.route("/crear")
+def crear():
     user = session.get('user')  # Obtiene el usuario si está en sesión
-    return render_template('index.html', user=user)
+    return render_template('crearMensaje.html', user=user)
+
 
 @app.route('/login')
 def login():
@@ -47,20 +80,6 @@ def crearPelicula():
     user = session.get('user')  # Obtiene el usuario si está en sesión
     return render_template('pelicula.html', user=user)
 
-@app.route('/crearSala')
-def crearSala():
-    user = session.get('user')  # Obtiene el usuario si está en sesión
-    return render_template('sala.html', user=user)
-
-@app.route('/crearProy')
-def crearProy():
-    user = session.get('user')  # Obtiene el usuario si está en sesión
-    return render_template('proyeccion.html', user=user)
-
-@app.route('/buscarPelicula')
-def buscarPelicula():
-    user = session.get('user')  # Obtiene el usuario si está en sesión
-    return render_template('buscar.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
